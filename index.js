72def34adb1db14687a76272cf9d158d29a243b2
mikemtzp <mikemp8226@gmail.com>
2022-05-09 15:47:34 -0500

    CSS menu functionality

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 //menuItem = menuItem
 //hamburger = hamburgerToggle
 
-const displayedMenu = document.querySelector('displayedMenu');
-const menuItems = document.querySelectorAll('menuItem');
-const hamburgerToggle = document.querySelector('hamburgerToggle');
-const menuIcon = document.querySelector('menuIcon');
-const closeIcon = document.querySelector('closeIcon');
+const displayedMenu = document.querySelector('.displayedMenu');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburgerToggle = document.querySelector('.hamburgerToggle');
+const menuIcon = document.querySelector('.menuIcon');
+const closeIcon = document.querySelector('.closeIcon');
 
-hamburgerToggle.addEventListener("click", function(){
-if (displayedMenu.classList.contains("showMenu")) {
-displayedMenu.classList.remove("showMenu");
-closeIcon.style.display = "none";
-menuIcon.style.display = "block";
-} else {
-    displayedMenu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+function openMenuHam() {
+    if (displayedMenu.classList.contains(".openMenu")) {
+    displayedMenu.classList.remove(".openMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    } else {
+        displayedMenu.classList.add(".openMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
 }
-});
+
+hamburgerToggle.addEventListener("click", openMenuHam)
