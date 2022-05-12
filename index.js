46ff8abecb95e4cfad9741569ac411038f36defa
Dafne Azzolina <dafne.azzolina@gmail.com>
2022-05-11 22:40:16 -0300

    Fix styling mistakes, add hover function, fix JS mistakes

--- index.js
+++ index.js
@@ -25,97 +25,92 @@ menuItems.forEach(
 );
 const projects = [
   {
-    name: "Multi-Post Stories",
+    name: 'Multi-Post Stories',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    image: "./css/Works/preview.png",
-    technology: ["HTML", "CSS", "Javascript"],
-    livelink: "#",
-    sourcelink: "#",
-    id: "btn1",
-    class:"firstCard"
-  },
-
- {
-    name: "Profesional Art Printing Data",
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    image: "./css/Works/preview.png",
-    technology: ["HTML", "CSS", "Javascript"],
-    livelink: "#",
-    sourcelink: "#",
-    id: "btn2",
-    class:"secondCard"
+    image: './css/Works/preview.png',
+    technology: ['HTML', 'CSS', 'Javascript'],
+    livelink: '#',
+    sourcelink: '#',
+    id: 'btn1',
+    class: 'firstCard',
   },
 
   {
-    name: "Data Dashboard Healthcare",
+    name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    image: "./css/Works/preview.png",
-    technology: ["HTML", "CSS", "Javascript"],
-    livelink: "#",
-    sourcelink: "#",
-    id: "btn3",
-    class:"thirdCard"
+    image: './css/Works/preview.png',
+    technology: ['HTML', 'CSS', 'Javascript'],
+    livelink: '#',
+    sourcelink: '#',
+    id: 'btn2',
+    class: 'secondCard',
   },
 
   {
-    name: "Website Protfolio",
+    name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    image: "./css/Works/preview.png",
-    technology: ["HTML", "CSS"],
-    livelink: "#",
-    sourcelink: "#",
-    id: "btn4",
-    class:"fourthCard"
+    image: './css/Works/preview.png',
+    technology: ['HTML', 'CSS', 'Javascript'],
+    livelink: '#',
+    sourcelink: '#',
+    id: 'btn3',
+    class: 'thirdCard',
   },
 
   {
-    name: "Profesional Art Printing Data",
+    name: 'Website Protfolio',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    image: "./css/Works/preview.png",
-    technology: ["HTML", "CSS", "Javascript"],
-    livelink: "#",
-    sourcelink: "#",
-    id: "btn5",
-    class:"fifthCard"
+    image: './css/Works/preview.png',
+    technology: ['HTML', 'CSS'],
+    livelink: '#',
+    sourcelink: '#',
+    id: 'btn4',
+    class: 'fourthCard',
   },
 
   {
-    name: "Data Dashboard Healthcare",
+    name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    image: "./css/Works/preview.png",
-    technology: ["HTML", "CSS", "Javascript", "Bootstrap"],
-    livelink: "#",
-    sourcelink: "#",
-    id: "btn6",
-    class:"sixthCard"
-  }
+    image: './css/Works/preview.png',
+    technology: ['HTML', 'CSS', 'Javascript'],
+    livelink: '#',
+    sourcelink: '#',
+    id: 'btn5',
+    class: 'fifthCard',
+  },
+
+  {
+    name: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    image: './css/Works/preview.png',
+    technology: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
+    livelink: '#',
+    sourcelink: '#',
+    id: 'btn6',
+    class: 'sixthCard',
+  },
 ];
 
+// Created body
+const body = document.querySelector('body');
+// Created "cards" div
+const cardDiv = document.createElement('div');
+cardDiv.classList.add('cards');
 
-
-
-const body = document.querySelector("body");
-const cardDiv = document.createElement("div");
-const secondPage = document.querySelector(".secondPage");
-cardDiv.classList.add("cards");
-const aboutMe = document.querySelector(".aboutMe");
+// Selected "About me" section
+const aboutMe = document.querySelector('.aboutMe');
+// Put "cards" div before "About me" section
 body.insertBefore(cardDiv, aboutMe);
 
+for (let i = 0; i < projects.length; i += 1) {
+  const secondItem = document.createElement('div');
+  secondItem.classList.add('secondItem');
+  secondItem.classList.add(projects[i].class);
+  cardDiv.appendChild(secondItem);
 
-for (let i=0; i<projects.length; i++) {
-/* const section = document.createElement("section"); */
-/* section.classList.add("centeredDynamics"); */
-
-const secondItem = document.createElement("div");
-secondItem.classList.add("secondItem");
-secondItem.classList.add(projects[i].class);
-cardDiv.appendChild(secondItem);
-/* body.insertBefore(section, aboutMe); */
-
-
-
-secondItem.innerHTML = `
+  secondItem.innerHTML = `
       <div class="descriptions">
+      <div class="hidden">
       <h2 class="bigText">${projects[i].name}</h2>
       <p class="smallText">${projects[i].description}</p>
       <ul class="socials">
@@ -123,49 +118,59 @@ secondItem.innerHTML = `
           <li class="boxes">${projects[i].technology[1]}</li>
           <li class="boxes">${projects[i].technology[2]}</li>
       </ul>
+      </div>  
   </div>
-      <button type="button" class="regButton">See Project</button>
 `;
+  const regButton = document.createElement('button');
+  regButton.type = 'button';
+  regButton.classList.add('regButton');
+  regButton.textContent = 'See Project';
+  secondItem.appendChild(regButton);
 }
 
-const open = document.querySelectorAll(".regButton");
-for (let i=0; i<projects.length; i++) {
-  open[i].addEventListener('click', ()=> {
-      console.log("clicked");
-      const main = document.createElement("div");
-      main.className = "main";
-      const popup = document.createElement("div");
-      popup.className = "popup";
-      popup.innerHTML = `
+document.querySelectorAll('.secondItem').forEach((item) => {
+  item.addEventListener('mouseenter', (e) => {
+    e.target.lastChild.classList.add('hiddenButton');
+  });
+
+  item.addEventListener('mouseleave', (e) => {
+    e.target.lastChild.classList.remove('hiddenButton');
+  });
+});
+
+const open = document.querySelectorAll('.regButton');
+for (let i = 0; i < projects.length; i += 1) {
+  open[i].addEventListener('click', () => {
+    const popup = document.createElement('div');
+    popup.className = 'popupDesktop';
+    popup.innerHTML = `
       <div class="popup">
       <div class="popupIntro">
         <h2 class="popupName">${projects[i].name}</h2>
         <button class="popupClose">&times;</button>
       </div>
-      <div class="popupButtons">
         <ul class="popupButtons">
           <li class="listItemPopup">${projects[i].technology[0]}</li>
           <li class="listItemPopup">${projects[i].technology[1]}</li>
           <li class="listItemPopup">${projects[i].technology[2]}</li>
         </ul>
-      </div>
+      <div class="flexPop">
       <img src=${projects[i].image} class="popupImg">
       <p class="popupDescription">${projects[i].description}</p>
       <div class="popupSources">
         <button class="popupLive"><a href="${projects[i].livelink}">See Live</a><img src="./css/icons/Live.png" alt=""></button>
-        <button class="popupSource"><a href="${projects[i].livelink}">See Live</a><img src="./css/icons/GitHub-popup.png" alt=""></button>
+        <button class="popupSource"><a href="${projects[i].livelink}">See Source</a><img src="./css/icons/GitHub-popup.png" alt=""></button>
+      </div>
       </div>
     </div>
     <div id="blurry"></div>
       `;
-  
-      main.appendChild(popup);
-      body.appendChild(main);
-  
-      const close = document.querySelector(".popupClose");
-      close.addEventListener("click", ()=> {
-          console.log("closed");
-          body.removeChild(main);
-      });
+
+    body.appendChild(popup);
+
+    const close = document.querySelector('.popupClose');
+    close.addEventListener('click', () => {
+      body.removeChild(popup);
+    });
   });
-};
+}
